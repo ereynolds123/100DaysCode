872d029419fcacaf092ee9cb8a7f9971b9839800
Clifford Reynolds <ereynold@estee.com>
2021-12-20 09:28:20 -0500

    To do items adding to the console.

---
+++ To Do/script.js
@@ -0,0 +1,7 @@
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    var data = new FormData(form);
+    for (const [name,value] of data) {
+      console.log(name,value)
+    }
+  })
