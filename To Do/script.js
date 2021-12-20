0e0bde0b00b10f85e22d8024e928f268889ad235
Clifford Reynolds <ereynold@estee.com>
2021-12-20 09:35:16 -0500

    Submit Form Refactored

--- To Do/script.js
+++ To Do/script.js
@@ -1,7 +1,9 @@
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    var data = new FormData(form);
-    for (const [name,value] of data) {
-      console.log(name,value)
-    }
-  })
+function submitForm(){
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        var data = new FormData(form);
+        for (const [name,value] of data) {
+          console.log(name,value)
+        }
+      })
+}
