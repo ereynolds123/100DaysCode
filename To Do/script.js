b64f13a80a019064fb66d40a8eefb57aa29988f5
Clifford Reynolds <ereynold@estee.com>
2021-12-21 13:03:10 -0500

    Has a bug of dplicating the 2nd task

--- To Do/script.js
+++ To Do/script.js
@@ -1,41 +1,30 @@
-// function submitForm(){
-//     form.addEventListener("submit", function(e) {
-//         e.preventDefault();
-//         var data = new FormData(form);
-//         for (const [name,value] of data) {
-//           console.log(name,value)
-          
-//         }
-//         //form.reset()
-//       })
-      
-      
-// }
-
-
+//Submits the form
 function submitForm(){
     form.addEventListener("submit", function(e){
         e.preventDefault();
         taskData = document.getElementById("activity").value;
-        // console.log("To do: "+ taskData)
         addingTask()
         return taskData
     })
     
 }
 
+//Adds a task element
 function addingTask(){
-    let taskElement = document.createElement("h3");
+    taskElement = document.createElement("h3");
     deleteButton = document.createElement("Button");
     taskElement.innerHTML = taskData;
+    taskElement.id = "taskElement"
     deleteButton.innerHTML ="Delete";
     deleteButton.id ="deleteButton";
+    deleteButton.addEventListener("click", deletingTask)
     document.body.appendChild(taskElement)
     document.body.appendChild(deleteButton)
 }
 
 
-
+//Removes a task element
 function deletingTask(){
-    console.log("Deleted")
+    taskElement.remove()
+    deleteButton.remove()
 }
