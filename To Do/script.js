719b217591ef20e0a331771456309be3104e36eb
Clifford Reynolds <ereynold@estee.com>
2021-12-21 12:50:21 -0500

    Adds an element and a delete button.

--- To Do/script.js
+++ To Do/script.js
@@ -12,13 +12,30 @@
       
 // }
 
+
 function submitForm(){
     form.addEventListener("submit", function(e){
         e.preventDefault();
-        var taskData = document.getElementById("activity").value;
-        console.log("To do: "+ taskData)
-        
+        taskData = document.getElementById("activity").value;
+        // console.log("To do: "+ taskData)
+        addingTask()
+        return taskData
     })
     
 }
 
+function addingTask(){
+    let taskElement = document.createElement("h3");
+    deleteButton = document.createElement("Button");
+    taskElement.innerHTML = taskData;
+    deleteButton.innerHTML ="Delete";
+    deleteButton.id ="deleteButton";
+    document.body.appendChild(taskElement)
+    document.body.appendChild(deleteButton)
+}
+
+
+
+function deletingTask(){
+    console.log("Deleted")
+}
