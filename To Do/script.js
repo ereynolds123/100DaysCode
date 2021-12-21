b8c266dc9ddc24c0679bb0e9e7b9b10d50a020cf
Clifford Reynolds <ereynold@estee.com>
2021-12-21 12:32:23 -0500

    Input data returning to the console. Cannot reset form

--- To Do/script.js
+++ To Do/script.js
@@ -1,15 +1,24 @@
-function submitForm(){
-    form.addEventListener("submit", function(e) {
-        e.preventDefault();
-        var data = new FormData(form);
-        for (const [name,value] of data) {
-          console.log(name,value)
+// function submitForm(){
+//     form.addEventListener("submit", function(e) {
+//         e.preventDefault();
+//         var data = new FormData(form);
+//         for (const [name,value] of data) {
+//           console.log(name,value)
           
-        }
-        form.reset()
-      })
+//         }
+//         //form.reset()
+//       })
       
       
-}
+// }
 
+function submitForm(){
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        var taskData = document.getElementById("activity").value;
+        console.log("To do: "+ taskData)
+        
+    })
+    
+}
 
