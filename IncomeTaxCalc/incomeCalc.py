6d1c863144d8a3013b8ba5745b668c8aec9cb361
Clifford Reynolds <ereynold@estee.com>
2021-12-22 10:14:09 -0500

    Calculating gross pay, amount of tax, take home pay

--- IncomeTaxCalc/incomeCalc.py
+++ IncomeTaxCalc/incomeCalc.py
@@ -1,9 +1,14 @@
 # A program to calculate take home pay after taxes and deductions
 
 #Inputs
-hoursWorked = int(input("Enter the amount of hours worked: "))
+hoursWorked = float(input("Enter the amount of hours worked: "))
 payRate = 58
+taxRate = 0.16
 
 #Calculates pay
-totalPay = hoursWorked * payRate
-print(totalPay)
+grossPay = hoursWorked * payRate 
+amountOfTax = grossPay * taxRate
+takeHomePay = grossPay -amountOfTax
+print(grossPay)
+print(amountOfTax)
+print(takeHomePay)
