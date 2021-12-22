f77742f313a77ec1ff28b4bc8124ece421feb398
Clifford Reynolds <ereynold@estee.com>
2021-12-22 13:10:34 -0500

    To do list close to working

--- To Do/script.js
+++ To Do/script.js
@@ -1,44 +1,28 @@
 //Submits the form
-function submitForm(){
-  const container = document.getElementById("container")
-    form.addEventListener("submit", function(e){
-        e.preventDefault();
-        taskData = document.getElementById("activity").value;
-        addingTask(taskData)
-        form.reset()
-        return taskData
-    });
-  
-    
-}
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  taskData = document.getElementById('activity').value;
+  addingTask(taskData);
+  form.reset(taskData);
+
+  return taskData;
+});
 
 //Adds a task element
-function addingTask(taskData){
-    taskElement = document.createElement("h3");
-    deleteButton = document.createElement("Button");
-    taskElement.innerHTML = taskData;
-    taskElement.id = "taskElement"
-    deleteButton.innerHTML ="Delete";
-    //deleteButton.id = taskData;
-    deleteButton.addEventListener("click", deletingTask)
-    document.body.appendChild(taskElement)
-    document.body.appendChild(deleteButton)
-    
+function addingTask(taskData) {
+  taskElement = document.createElement('h3');
+  deleteButton = document.createElement('Button');
+  taskElement.innerHTML = taskData;
+  taskElement.id = 'taskElement';
+  deleteButton.innerHTML = 'Delete';
+  //deleteButton.id = taskData;
+  deleteButton.addEventListener('click', deletingTask);
+  document.body.appendChild(taskElement);
+  document.body.appendChild(deleteButton);
 }
 
-function deletingTask(){
-  container.addEventListener("click", function(e) {
-    const tgt = e.target;
-    if (tgt.classList.contains("delete")) {
-     //Removes a task element
-      tgt.closest("div").remove();
-    }
-  })
+//Removes a task element
+function deletingTask() {
+  taskElement.remove();
+  deleteButton.remove();
 }
-
-
-// //Removes a task element
-// function deletingTask(){
-//     taskElement.remove()
-//     deleteButton.remove()
-// }
