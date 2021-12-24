de9f8113daa2fe6d3ed74d67b223370147986bfb
Emily Reynolds <emilyreynoldsdsa@gmail.com>
2021-12-24 18:31:42 -0500

    Clock working.

--- Clock/script.js
+++ Clock/script.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+
+//Creates a clock element. Updates every second
+function myTimer() {
+    const date = new Date();
+  document.getElementById("clock").innerHTML = date.toLocaleTimeString();
+  }
+
+setInterval(myTimer, 1000);
